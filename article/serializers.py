b03fdcd0e8261213ce3c32ac0db7eb85b0e2fea3
Moonmoo <ansaudwn1234@gmail.com>
2022-07-05 13:40:24 +0900

    날짜 수정

--- article/serializers.py
+++ article/serializers.py
@@ -52,7 +52,7 @@ class ArticleSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ArticleModel
-        fields = ['id','author','title','image','contents','output','comment_set']
+        fields = ['id','author','title','image','contents','output','comment_set','exposure_start_date']
 
 class ArticleImageSerializer(serializers.ModelSerializer):
     class Meta:
